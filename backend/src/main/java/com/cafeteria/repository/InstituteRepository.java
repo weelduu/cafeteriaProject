0069286e889fcdbe6f97781c86e0de24098388c7
eduard.tibor <eduard.tibor@ext.soprasteria.com>
2026-04-27 13:45:09 +0200

    feat(): add InstituteRepository

---
+++ backend/src/main/java/com/cafeteria/repository/InstituteRepository.java
@@ -0,0 +1,9 @@
+package com.cafeteria.repository;
+
+import com.cafeteria.model.Institute;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstituteRepository extends JpaRepository<Institute, Long> {
+}
